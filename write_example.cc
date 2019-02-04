9c15050976619784a8e21b759e910c2cfde5f881
Rodrigo Tobar <rtobar@icrar.org>
2019-02-04 17:46:57 +0800

    Rewriting write_example
    This new version accepts more command line parameters to alter the
    different execution options involved in writing to the casa table. The
    new options include the cell size, the number of dimensions to use for
    the cell (only 1 and 2 are supported at the moment), the data type (with
    float, double, complex and dcomplex being supported), and the number of
    rows to write. The default values for all of these options are those
    previously hardcoded in the code.
    
    The code has also been rearranged a bit to make exception handling a bit
    easier, use a random value to fill the data arrays, and have easier
    memory handling.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- write_example.cc
+++ write_example.cc
@@ -1,97 +1,175 @@
+#include <memory>
+#include <random>
+
+#include <adios2.h>
+
 #include "common.h"
 
-size_t rows = 10000;
-size_t columns = 1;
-size_t cell_size = 1024;
+struct options {
+    int mpi_rank = 0;
+    int mpi_size = 1;
+    size_t columns = 1;
+    size_t rows = 10000;
+    size_t cell_size = 1;
+    int n_dims = 1;
+    std::string filename;
+    std::string stman_type {"none"};
+    std::string data_type {"float"};
+};
 
-int main(int argc, char **argv){
-
-
-    int mpiRank, mpiSize;
-    MPI_Init(&argc,&argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
-    MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
-
-    try{
-
-
-    string stman_type = argv[1];
-    string filename = argv[2];
-
-    if(mpiRank ==0)
-    {
-        cout << "rows = " << rows << ", columns = " << columns << ", cell size = "  << cell_size << ", total bytes = "  << cell_size*rows*columns*sizeof(float) << ", stman = " << stman_type << ", filename = " << filename << endl;
-    }
-
-    DataManager *stman;
-    {
-        if(stman_type == "Adios2StMan")
-        {
-            adios2::Params engineParams = {};
-            vector<adios2::Params> transportParams;
-            string engineType = "BPFile";
-            stman = new Adios2StMan(MPI_COMM_WORLD, engineType, engineParams, transportParams);
-        }
-        else if(stman_type == "Hdf5StMan")
-        {
-            stman = new Hdf5StMan;
-        }
-        else if(stman_type == "AdiosStMan")
-        {
-            stman = new AdiosStMan("POSIX", "verbose=0", 1000, 1);
-        }
-
-        IPosition array_pos = IPosition(1,cell_size);
-
-        TableDesc td("", "1", TableDesc::Scratch);
-
-        for(size_t i=0; i<columns; ++i)
-        {
-            string column_name = "column" + to_string(i);
-            td.addColumn (ArrayColumnDesc<Float>(column_name, array_pos, ColumnDesc::FixedShape));
-        }
-
-        SetupNewTable newtab(filename, td, Table::New);
-        if(stman_type != "none")
-        {
-            newtab.bindAll(*stman);
-        }
-        Table tab(MPI_COMM_WORLD, newtab, rows);
-
-        vector<ArrayColumn<Float>> colvec;
-        for(size_t i=0; i<columns; ++i)
-        {
-            string column_name = "column" + to_string(i);
-            colvec.emplace_back(tab, column_name);
-        }
-
-        Array<Float> arr_Float(array_pos);
-
-        GenData(arr_Float, array_pos, mpiRank);
-
-        for(int i=mpiRank; i<rows; i+=mpiSize)
-        {
-            for(auto &k : colvec)
-            {
-                k.put(i, arr_Float);
-            }
-        }
-    }
-
-    if(stman_type != "none")
-    {
-        delete stman;
-    }
-
-
-    }
-    catch(std::exception &e){
-        std::cout << e.what() << std::endl;
-    }
-
-    MPI_Finalize();
-
-    return 0;
+template<typename T>
+void fill_array(Array<T> &array)
+{
+    array = T (std::random_device()());
 }
 
+template<typename C>
+void fill_array_complex(Array<C> &array)
+{
+    using ctype = typename C::value_type;
+    std::random_device dev;
+    array = C {ctype(dev()), ctype(dev())};
+}
 
+template<>
+void fill_array<Complex>(Array<Complex> &array)
+{
+    fill_array_complex<Complex>(array);
+}
+
+template<>
+void fill_array<DComplex>(Array<DComplex> &array)
+{
+    fill_array_complex<DComplex>(array);
+}
+
+template<typename T>
+void write_to_table(const options &opts, IPosition &array_pos, std::unique_ptr<DataManager> &&stman)
+{
+    if (opts.mpi_rank == 0)
+    {
+        std::cout << "rows = " << opts.rows << ", columns = " << opts.columns
+                  << ", cell size = "  << opts.cell_size
+                  << ", total bytes = "
+                  << opts.cell_size * opts.rows * opts.columns * sizeof(T)
+                  << ", stman = " << opts.stman_type << ", filename = "
+                  << opts.filename << '\n';
+    }
+
+    TableDesc td("", "1", TableDesc::Scratch);
+
+    for (size_t i = 0; i < opts.columns; ++i)
+    {
+        string column_name = "column" + to_string(i);
+        td.addColumn (ArrayColumnDesc<T>(column_name, array_pos, ColumnDesc::FixedShape));
+    }
+
+    SetupNewTable newtab(opts.filename, td, Table::New);
+    if (stman)
+    {
+        newtab.bindAll(*stman);
+    }
+    Table tab(MPI_COMM_WORLD, newtab, opts.rows);
+
+    vector<ArrayColumn<T>> colvec;
+    for (size_t i = 0; i < opts.columns; ++i)
+    {
+        std::string column_name = "column" + to_string(i);
+        colvec.emplace_back(tab, column_name);
+    }
+
+    // Create some data and write it
+    Array<T> arr(array_pos);
+    fill_array(arr);
+    for (size_t i = opts.mpi_rank; i < opts.rows; i += opts.mpi_size)
+    {
+        for (auto &k : colvec)
+        {
+            k.put(i, arr);
+        }
+    }
+}
+
+void run(int argc, char **argv){
+
+    options opts;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &opts.mpi_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &opts.mpi_size);
+
+    opts.stman_type = argv[1];
+    opts.filename = argv[2];
+    if (argc > 3) {
+      opts.cell_size = std::stold(argv[3]);
+    }
+    if (argc > 4) {
+      opts.n_dims = std::stod(argv[4]);
+    }
+    if (argc > 5) {
+      opts.data_type = argv[5];
+    }
+    if (argc > 6) {
+      opts.rows = std::stold(argv[6]);
+    }
+
+    // The storage manager to use
+    std::unique_ptr<DataManager> stman;
+    if (opts.stman_type == "Adios2StMan")
+    {
+        adios2::Params engineParams = {};
+        vector<adios2::Params> transportParams;
+        string engineType = "BPFile";
+        stman = std::make_unique<Adios2StMan>(MPI_COMM_WORLD, engineType, engineParams, transportParams);
+    }
+#ifdef HAS_HDF5STMAN
+    else if(stman_type == "Hdf5StMan")
+    {
+        stman = std::make_unique<Hdf5StMan>(Hdf5StMan);
+    }
+#endif
+#ifdef HAS_ADIOSSTMAN
+    else if(stman_type == "AdiosStMan")
+    {
+        stman = std::make_unique<AdiosStMan>("POSIX", "verbose=0", 1000, 1);
+    }
+#endif
+
+    // The dimensionality
+    auto array_pos = std::make_unique<IPosition>(1, opts.cell_size);
+    if (opts.n_dims == 2) {
+        array_pos = std::make_unique<IPosition>(2, 1, opts.cell_size);
+    }
+    else if (opts.n_dims > 2) {
+        throw std::runtime_error("Only 1 or 2 dimensions supported in writer_example");
+    }
+
+    // The data type
+    if (opts.data_type == "float") {
+        write_to_table<Float>(opts, *array_pos, std::move(stman));
+    }
+    else if (opts.data_type == "double") {
+        write_to_table<Double>(opts, *array_pos, std::move(stman));
+    }
+    else if (opts.data_type == "complex") {
+        write_to_table<Complex>(opts, *array_pos, std::move(stman));
+    }
+    else if (opts.data_type == "dcomplex") {
+        write_to_table<DComplex>(opts, *array_pos, std::move(stman));
+    }
+    else {
+        std::runtime_error("Only float/double/complex/dcomplex data types supported in writer_example");
+    }
+}
+
+int main(int argc, char **argv)
+{
+    try {
+        MPI_Init(&argc, &argv);
+        run(argc, argv);
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    MPI_Finalize();
+    return 0;
+}
