d77fd9be39e47ccee558f2317cf8339b066c0564
Rodrigo Tobar <rtobar@icrar.org>
2019-02-04 15:41:24 +0800

    New compare_sizes.sh benchmarking script
    This script explores the parameter space defined by the combination of
    different storage managers, row counts, cell sizes, cell dimensionality
    and data types and compares the expected and real sizes of the generated
    table directories.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

---
+++ compare_sizes.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+rm -rf output
+mkdir output
+
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 <nrows>" 1>&2
+	exit 1
+fi
+rows=$1
+
+echo "      stman    dtype dims cellsize expect [MB] real [MB] rel. diff [%]"
+echo "=========== ======== ==== ======== =========== ========= ============="
+for stman in none Adios2StMan; do
+	for dtype in float double complex dcomplex; do
+		for dims in 1 2; do
+			for cellsize in 4 8 16 32 64 128 256 512 1024; do
+
+				output=output/${stman}_${dtype}_${dims}d_${cellsize}
+				msg=`./write_example $stman ${output} $cellsize $dims $dtype $rows`
+
+				if [ $? -ne 0 ]; then
+					echo "Failure during execution of write_example: $msg" 1>&2
+					exit 1
+				fi
+
+				expected=`echo $msg | sed 's/.*total bytes = \([0-9]\+\).*/\1/'`
+				real=`du -bs $output | sed 's/\([0-9]\+\).*/\1/'`
+				diff=`echo "scale=2; ($real - $expected) * 100. /$expected" | bc`
+				expected=`echo "scale=2; $expected / 1024. / 1024" | bc`
+				real=`echo "scale=2; $real / 1024. / 1024" | bc`
+				printf "%11s %8s %4d %8d %11s %9s %13s\n" $stman $dtype $dims $cellsize $expected $real $diff
+			done
+		done
+	done
+done
